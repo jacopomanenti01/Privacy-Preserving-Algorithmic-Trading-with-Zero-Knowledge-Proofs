5bf457321e888010a973910680f2eefc72ec41ec
jacopomanenti01 <jacopomanenti01@gmail.com>
2025-01-06 19:28:59 +0100

    created classes for trading bot

---
+++ backend/utils/historical_bars.py
@@ -0,0 +1,53 @@
+# Alpaca api for stocks
+from alpaca.data import StockHistoricalDataClient, StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
+
+# Alpaca api for crypto
+
+from alpaca.data.live.crypto import CryptoDataStream
+from alpaca.data.historical.crypto import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest
+
+import datetime
+import pandas as pd
+
+
+
+class GetHistoricalBars():
+
+    def __init__(self, ticker, api_key,api_secret ):
+        self.ticker = ticker
+        self.params = None
+        self.stock_data_client = StockHistoricalDataClient(api_key, api_secret)
+
+
+    def build_params(self, delta):
+        end = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=20)
+        print(end)
+        request_params = StockBarsRequest(
+            symbol_or_symbols=self.ticker,
+            timeframe = TimeFrame.Minute,
+            start = end - datetime.timedelta(minutes=delta),
+            end = end
+            )
+        self.params=request_params
+        
+        '''
+        
+        request_params = CryptoBarsRequest(
+            symbol_or_symbols=ticker,
+            timeframe = TimeFrame.Minute,
+            start = end - datetime.timedelta(minutes=100),
+            end = end
+            )
+        
+        bars = cryto_data_client.get_crypto_bars(request_params=request_params)
+
+        '''  
+    def retreive_historical_bars(self, delta):  
+        self.build_params(delta)
+        bars = self.stock_data_client.get_stock_bars(request_params=self.request_params)
+        bars_pd = pd.DataFrame(dict(bar) for bar in bars[self.ticker])
+
+        print(f"historical dataframe: \n{bars_pd}")
+        return bars_pd
